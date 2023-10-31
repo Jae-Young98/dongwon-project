40b4a5181cd0963a7d76188f65f56a0ac8118c8a
JaeYoung <ygy0102@gmail.com>
2023-10-31 16:16:17 +0900

    feat: 실행 버튼 클릭시 서버에 전송하는 기능 구현

--- src/main/resources/static/scripts/basic.js
+++ src/main/resources/static/scripts/basic.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
-    var fileTarget = $('.files .upload-hidden');
+    const fileTarget = $('.files .upload-hidden');
 
-    fileTarget.on('change', function(){  // 값이 변경되면
+    // 값이 변경되면
+    fileTarget.on('change', function(){
+        let filename;
         if(window.FileReader){  // modern browser
-            var filename = $(this)[0].files[0].name;
+            filename = $(this)[0].files[0].name;
         }
         else {  // old IE
-            var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+            filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
         }
 
         // 추출한 파일명 삽입
         $(this).siblings('.upload-name').val(filename);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const confirmButton = document.getElementById('confirm-button');
+    const uploadForm = document.getElementById('upload-form');
+    const fileInput = document.getElementById('filename');
+
+    confirmButton.addEventListener('click', function () {
+        if (fileInput.files.length === 0) {
+            alert("선택된 파일이 없습니다. 파일을 선택해 주세요.");
+        } else {
+            const fileName = fileInput.files[0].name;
+            if (confirm("주의! 올바른 파일이 맞습니까?\n" + fileName)) {
+                uploadForm.submit();
+            }
+        }
+    })
+})
 
 function validateInput(inputElem) {
     const notification = document.getElementById("notification");
